853e99720aa85e5734bd9fe473a1f1d997c8ab87
tandang <cuctrau159@gmail.com>
2023-01-21 00:26:08 +0700

    fix ssl server

--- src/app/index.js
+++ src/app/index.js
@@ -1,44 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const useragent = require("express-useragent")
-const routes = require('../routes');
-const https = require('https');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const useragent = require("express-useragent");
+const routes = require("../routes");
+const https = require("https");
+const fs = require("fs");
+var path = require('path');
+require("dotenv").config();
 // const FoodType = require('../_models/FoodType');
 // const Food = require('../_models/Food');
 
+const key = fs.readFileSync(path.resolve('ssl/server-key.pem'));
+const cert = fs.readFileSync(path.resolve('ssl/server-cert.pem'));
+const options = {
+  key: key,
+  cert: cert
+};
+
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 // connect to database
-const db = require('../config/database');
+const db = require("../config/database");
 
 db.sync({ alter: true });
-
-// db.sync().then(() => {
-//     console.log("Database connected");
-// }).catch((err) => {
-//     console.log("Database not connected",err);
-// });
-// const models = initModels(db);
-
-
-// models.Customer.findAll().then((data) => {
-//     console.log(data);
-// }).catch((err) => {
-//     console.log(err);
-// });
-
-
-// models.Ward.sync({focus:false}).then(() => {
-//     console.log("District table created");
-// }).catch((err) => {
-//     console.log("District table not created",err);
-// });
-
-
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors({ origin: true, credentials: true }));
@@ -47,15 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 routes(app);
 
-const options = {
-    key: fs.readFileSync('../../ssl/server-key.pem'),
-    cert: fs.readFileSync('../../ssl/server-cert.pem'),
-};
+
 
 https.createServer(options, app).listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
 
 // app.listen(port, () => {
 //     console.log(`Example app listening at http://localhost:${port}`)
